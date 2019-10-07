b1a2187269ca0f27832961f8e0b166dbdde2891a
Trevor N Trusty <tsquared21@outlook.com>
2019-10-07 19:20:44 -0400

    Added int to char function, chaned output of tic tac toe board for easier readability, and gave each spot a number

--- game.cpp
+++ game.cpp
@@ -1,12 +1,49 @@
 #include "game.h"
 #include "player.h"
-#include "iostream"
+#include <iostream>
+#include <string>
+
+char intToChar(int num)
+{
+	switch (num)
+	{
+	case 1:
+		return '1';
+		break;	
+	case 2:
+		return '2';
+		break;	
+	case 3:
+		return '3';
+		break;
+	case 4:
+		return '4';
+		break;
+	case 5:
+		return '5';
+		break;
+	case 6:
+		return '6';
+		break;
+	case 7:
+		return '7';
+		break;
+	case 8:
+		return '8';
+		break;
+	case 9:
+		return '9';
+		break;
+	}
+	return 0;
+}
 
 void Game::initialize(char array[])
 {
 	for (int i = 1; i <= 9; i++)
 	{
-		array[i] = '-';
+		//array[i] = '-';
+		array[i] = intToChar(i);
 	}
 }
 
@@ -14,17 +51,18 @@ void Game::showBoard(char game[])
 {
 	for (int i = 1; i <= 3; i++)
 	{
-		std::cout << game[i];
+		std::cout << "| " << game[i] << " | ";
 	}
 	std::cout << std::endl;
 	for (int i = 4; i <= 6; i++)
 	{
-		std::cout << game[i];
+		std::cout << "| " << game[i] << " | ";
 	}
 	std::cout << std::endl;
 	for (int i = 7; i <= 9; i++)
 	{
-		std::cout << game[i];
+		std::cout << "| " << game[i] << " | ";
+
 	}
 	std::cout << std::endl;
 }
@@ -33,7 +71,11 @@ bool Game::inGame(char m[])
 {
 	for (int i = 1; i <= 9; i++)
 	{
-		if (m[i] == '-')
+		//if (m[i] == '-')
+		//{
+		//	return true;
+		//}
+		if (m[i] >= 1 || m[i] <= 9)
 		{
 			return true;
 		}
--- player.cpp
+++ player.cpp
@@ -1,5 +1,10 @@
 #include "player.h"
 
+
+// 1  2  3
+// 4  5  6
+// 7  8  9
+
 Player::Player()
 {
 	setMark('0');
@@ -29,11 +34,17 @@ char Player::getMark() const
 
 bool Player::playerWin(char m[])
 {
+	bool horizontal = (m[1] == m[2] && m[2] == m[3] && m[3] == mark) || (m[4] == m[5] && m[5] == m[6] && m[6] == mark) || (m[7] == m[8] && m[8] == m[9] && m[9] == mark);
+	bool vertical = (m[1] == m[4] && m[4] == m[7] && m[7] == mark) || (m[2] == m[5] && m[5] == m[8] && m[8] == mark) || (m[3] == m[6] && m[6] == m[9] && m[9] == mark);
+	bool diagonal = (m[1] == m[5] && m[5] == m[9] && m[9] == mark) || (m[3] == m[5] && m[5] == m[5] && m[7] == mark);
 
-	if (((m[1] == m[2]) && (m[2] == m[3]) && (m[3] == mark)) || (((m[4] == m[5]) && (m[5] == m[6]) && (m[6] == mark)))
-		|| (((m[7] == m[8]) && (m[8] == m[9]) && (m[9] == mark)) || (((m[1] == m[5]) && (m[5] == m[9]) && (m[9] == mark)))) // 7, 8 and 9....1, 5, and 9
-		|| (((m[1] == m[2]) && (m[2] == m[3]) && (m[3] == mark)) || (((m[4] == m[5]) && (m[5] == m[6]) && (m[6] == mark)))) // 2, 5, and 8..... 3, 5, and 7
-		|| (((m[1] == m[4]) && (m[4] == m[7]) && (m[7] == mark)) || (((m[3] == m[6]) && (m[6] == m[9]) && (m[9] == mark))))) // 1, 4, and 7.... 3, 6, and 9
+	//bool win = horTop || horMiddle || horBottom || vertLeft || vertMiddle || vertRight || diagDown || diagUp;
+
+	//if (((m[1] == m[2]) && (m[2] == m[3]) && (m[3] == mark)) || (((m[4] == m[5]) && (m[5] == m[6]) && (m[6] == mark)))
+	//	|| (((m[7] == m[8]) && (m[8] == m[9]) && (m[9] == mark)) || (((m[1] == m[5]) && (m[5] == m[9]) && (m[9] == mark)))) // 7, 8 and 9....1, 5, and 9
+	//	|| (((m[2] == m[5]) && (m[5] == m[8]) && (m[8] == mark)) || (((m[3] == m[5]) && (m[5] == m[7]) && (m[7] == mark)))) // 2, 5, and 8..... 3, 5, and 7
+	//	|| (((m[1] == m[4]) && (m[4] == m[7]) && (m[7] == mark)) || (((m[3] == m[6]) && (m[6] == m[9]) && (m[9] == mark))))) // 1, 4, and 7.... 3, 6, and 9
+	if(horizontal || vertical || diagonal)
 	{
 		return true;
 	}
